4b75319466e76956c83afc304275c02b931f0bbf
rubis-vladimir <84345727+rubis-vladimir@users.noreply.github.com>
2022-04-21 20:34:46 +0300

    Initial Commit

---
+++ BurgerHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BurgerHouse
+//
+//  Created by Владимир Рубис on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
